c8da946c8b05a3252e29f44c29a4099b8543f7e3
Diogo Neves <neo16pt@gmail.com>
2025-03-05 22:29:26 +0000

    initial html mockups

---
+++ script.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Serve static files from the root directory
+app.use(express.static("./"));
+
+// Serve index.html for the root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Serve index.html for the root route
+app.get("/tournaments", (req, res) => {
+  res.sendFile(path.join(__dirname, "tournaments.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
