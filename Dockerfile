438ae9cdbd0dcfd16d17ad799e33832e673e51a8
Diogo Neves <neo16pt@gmail.com>
2025-03-07 20:05:56 +0000

    can now debug inside the container

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,4 @@ COPY package*.json ./
 
 RUN npm install
 
-COPY . .
-
 RUN npm install -g nodemon
-
-EXPOSE 3000
-EXPOSE 9229
--- app.js
+++ app.js
@@ -7,16 +7,17 @@ app.use(express.static("./"));
 
 // Serve index.html for the root route
 app.get("/", (req, res) => {
-  console.log(`Getting index.`);
+  console.log(`Getting index. My man.`);
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Serve index.html for the root route
+// Serve tournaments.html for the tournaments route
 app.get("/tournaments", (req, res) => {
+  console.log(`Getting tournaments.`);
   res.sendFile(path.join(__dirname, "tournaments.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}. yay!`);
+  console.log(`Server running on port ${PORT}!`);
 });
